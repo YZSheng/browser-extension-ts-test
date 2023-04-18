34a1bcddd78f7ab4246832cf44fe566d89510985
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-04-18 10:54:19 +0800

    Add ChatGPT styling

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -3,6 +3,7 @@ import { sum } from "./sum";
 
 function init() {
   console.log(`I'm running inside a content script, 1 + 1 is ${sum(1, 1)}`);
+  document.body.classList.add("chatgic");
 }
 
 document.addEventListener("DOMContentLoaded", init);
