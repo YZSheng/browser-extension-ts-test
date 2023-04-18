322d27ed44177f596f1cecec475f977c3e16fd0f
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-04-18 17:27:00 +0800

    Add basic toggle in popup

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,35 +1,60 @@
 import "./style.css";
 import { sum } from "./sum";
 
-function gicize() {
+const logInput = () => {
+  const inputArea = document.querySelector("textarea");
+  console.log("Logging input...", inputArea?.value);
+};
+
+const listenOnKeydown = (event: KeyboardEvent) => {
+  if (event.key === "Enter") {
+    logInput();
+  }
+};
+
+const listenOnEnter = () => {
+  logInput();
+};
+
+const gicize = () => {
+  document.body.classList.add("chatgic");
   const h1 = document.getElementsByTagName("h1");
-  h1[0].innerText = "ChatGIC";
+  if (h1.length) {
+    h1[0].innerText = "ChatGIC";
+  }
   const inputArea = document.querySelector("textarea");
   const sendButton = document.querySelector("textarea + button");
 
-  const logInput = () => {
-    console.log(inputArea?.value);
-  };
+  inputArea?.addEventListener("keydown", listenOnKeydown);
 
-  inputArea?.addEventListener("keydown", (event: KeyboardEvent) => {
-    if (event.key === "Enter") {
-      logInput();
-    }
-  });
+  sendButton?.addEventListener("click", listenOnEnter);
+};
 
-  sendButton?.addEventListener("click", () => logInput());
-}
+const unGicize = () => {
+  document.body.classList.remove("chatgic");
+  const h1 = document.getElementsByTagName("h1");
+  if (h1.length) {
+    h1[0].innerText = "Chat GPT";
+  }
+  const inputArea = document.querySelector("textarea");
+  const sendButton = document.querySelector("textarea + button");
+
+  inputArea?.removeEventListener("keydown", listenOnKeydown);
+
+  sendButton?.removeEventListener("click", listenOnEnter);
+};
 
 function init() {
   console.log(`I'm running inside a content script, 1 + 1 is ${sum(1, 1)}`);
-  document.body.classList.add("chatgic");
-  const timer = setInterval(() => {
-    const h1 = document.getElementsByTagName("h1");
-    if (h1.length) {
+  chrome.runtime.onMessage.addListener((request, sender) => {
+    console.log("sender", sender);
+    console.log("request", request);
+    if (request.chatGICStatus) {
       gicize();
-      clearInterval(timer);
+    } else if (request.chatGICStatus === false) {
+      unGicize();
     }
-  }, 100);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", init);
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,25 +1,27 @@
 import React from "react";
 
-import logo from "@assets/img/logo.svg";
-
 export default function Popup(): JSX.Element {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p>Popup styled with TailwindCSS!</p>
-      </header>
+    <div className="wrapper">
+      <p>ChatGIC</p>
+      <label className="switch">
+        <input
+          type="checkbox"
+          className=""
+          onChange={async (event: React.ChangeEvent<HTMLInputElement>) => {
+            const tabs = await chrome.tabs.query({
+              active: true,
+              currentWindow: true,
+            });
+            if (tabs.length) {
+              chrome.tabs.sendMessage(tabs[0].id as number, {
+                chatGICStatus: event.target.checked,
+              });
+            }
+          }}
+        />
+        <span className="slider round"></span>
+      </label>
     </div>
   );
 }
