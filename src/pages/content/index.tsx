e3e65c2fa41ed0b4a72bbe6a136f39d63d2b5c44
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-04-18 11:06:34 +0800

    Change h1 text to ChatGIC

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -4,6 +4,13 @@ import { sum } from "./sum";
 function init() {
   console.log(`I'm running inside a content script, 1 + 1 is ${sum(1, 1)}`);
   document.body.classList.add("chatgic");
+  const timer = setInterval(() => {
+    const h1 = document.getElementsByTagName("h1");
+    if (h1.length) {
+      h1[0].innerText = "ChatGIC";
+      clearInterval(timer);
+    }
+  }, 100);
 }
 
 document.addEventListener("DOMContentLoaded", init);
