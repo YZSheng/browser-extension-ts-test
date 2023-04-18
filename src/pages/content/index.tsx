3c1b1c98888fad85efa6a712dea4a915b4d979af
Sheng Yunzhou <shengyunzhou@gmail.com>
2023-04-18 11:16:17 +0800

    Add event listener on input

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,13 +1,32 @@
 import "./style.css";
 import { sum } from "./sum";
 
+function gicize() {
+  const h1 = document.getElementsByTagName("h1");
+  h1[0].innerText = "ChatGIC";
+  const inputArea = document.querySelector("textarea");
+  const sendButton = document.querySelector("textarea + button");
+
+  const logInput = () => {
+    console.log(inputArea?.value);
+  };
+
+  inputArea?.addEventListener("keydown", (event: KeyboardEvent) => {
+    if (event.key === "Enter") {
+      logInput();
+    }
+  });
+
+  sendButton?.addEventListener("click", () => logInput());
+}
+
 function init() {
   console.log(`I'm running inside a content script, 1 + 1 is ${sum(1, 1)}`);
   document.body.classList.add("chatgic");
   const timer = setInterval(() => {
     const h1 = document.getElementsByTagName("h1");
     if (h1.length) {
-      h1[0].innerText = "ChatGIC";
+      gicize();
       clearInterval(timer);
     }
   }, 100);
